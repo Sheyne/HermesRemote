593f5c24de72f325ea69d9de4f92eb2deb183d31
Sheyne Anderson <me@sheyne.com>
2012-06-18 10:02:38 -0600

    Changed HRSettingViewController to HRConnectController to better reflect its usage.

---
+++ HermesRemote/HRConnectViewController.m
@@ -0,0 +1,36 @@
+//
+//  HRSettingViewController.m
+//  HermesRemote
+//
+//  Created by Sheyne Anderson on 6/17/12.
+//  Copyright (c) 2012 Sheyne Anderson. All rights reserved.
+//
+
+#import "HRConnectViewController.h"
+#import "HRState.h"
+
+@implementation HRConnectViewController
+
+-(void)viewDidLoad{
+    [[HRState sharedInstance] addObserver:self forKeyPath:@"isConnected" options:NSKeyValueObservingOptionNew context:NULL];
+}
+
+-(void)viewDidUnload{
+    [[HRState sharedInstance] removeObserver:self forKeyPath:@"isConnected"];
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
+    NSNumber *new = [change objectForKey:@"new"];
+    if (new.boolValue == YES) {
+        [self performSegueWithIdentifier:@"Connect" sender:self];
+    } else {
+        [self.navigationController popToViewController:self animated:YES];
+    }
+}
+
+-(IBAction)connect:(id)sender{
+    [[HRState sharedInstance] connectToServer:address.text onPort:port.text.intValue];
+    [self.view endEditing:YES];
+}
+
+@end
